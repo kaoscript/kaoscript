a08eaded8c42f4f35101312e020c4e3dea7d6656
Baptiste Augrain <daiyam@zokugun.org>
2019-03-07 23:12:01 +0100

    fix number

--- test/fixtures/compile/number/number.nice.js
+++ test/fixtures/compile/number/number.nice.js
@@ -5,4 +5,13 @@ module.exports = function() {
 	let binary = 661;
 	let radix = 50998809317;
 	let __ks_float_1 = 123456.789012;
+	let exponent_positive = -7.2e+22;
+	let exponent_negative = 1.1e-10;
+	let float_exponent = 1.23456e-65;
+	let float_dot = 0.789012;
+	let float_dot_exponent = 1000;
+	let float_hex1 = 16.0625;
+	let float_hex2 = 9;
+	let float_hex3 = 3.078125;
+	let float_hex4 = 10944;
 };
